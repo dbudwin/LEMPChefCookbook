5d8324f97a1e93a88919e5fcbf0bdfd57f1387c0
Drew Budwin <drew@budw.in>
2017-06-15 20:18:00 -0400

    Install composer for PHP dependencies

--- recipes/default.rb
+++ recipes/default.rb
@@ -1,3 +1,4 @@
 include_recipe 'LEMPChefCookbook::mysql'
 include_recipe 'LEMPChefCookbook::php'
 include_recipe 'LEMPChefCookbook::nginx'
+include_recipe 'composer::default'
--- test/smoke/default/default_test.rb
+++ test/smoke/default/default_test.rb
@@ -5,3 +5,7 @@ end
 describe package('git') do
   it { should be_installed }
 end
+
+describe command('composer') do
+  it { should exist }
+end
